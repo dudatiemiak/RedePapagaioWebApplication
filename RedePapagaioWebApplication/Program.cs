581ef4f07370b51bbdb44a4534eac4de0c3dcde7
dudatiemiak <rm554756@fiap.com.br>
2025-06-04 22:17:27 -0300

    Configurando o banco de dados

--- RedePapagaioWebApplication/Program.cs
+++ RedePapagaioWebApplication/Program.cs
@@ -1,3 +1,8 @@
+using RedePapagaioWebApplication.Connection;
+using Oracle.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+   options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
