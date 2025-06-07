6a35c9e0d3ffe8f90f7a99f69d9011a8aa8cb724
dudatiemiak <rm554756@fiap.com.br>
2025-06-07 03:41:47 -0300

    Arrumando configurações da aplicação

--- RedePapagaioWebApplication/Program.cs
+++ RedePapagaioWebApplication/Program.cs
@@ -1,31 +1,64 @@
-using RedePapagaioWebApplication.Connection;
-using Oracle.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using RedePapagaioWebApplication.Connection;
+using RedePapagaioWebApplication.Repository;
+using RedePapagaioWebApplication.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "API REST de Controle de Desastres Naturais (Ocorrências), Usuários, Ajudas.",
+        Description = "Uma aplicação destinada ao controle de ocorrências, usuários e ajudas fornecidas por esses usuários através de rotas de api e integração com o banco de dados"
+    });
+});
 
+builder.Services.AddScoped<AjudaRealizadaService>();
+builder.Services.AddScoped<RegiaoService>();
+builder.Services.AddScoped<NivelUrgenciaService>();
+builder.Services.AddScoped<OcorrenciaService>();
+builder.Services.AddScoped<StatusOcorrenciaService>();
+builder.Services.AddScoped<TelefoneService>();
+builder.Services.AddScoped<TipoAjudaService>();
+builder.Services.AddScoped<TipoOcorrenciaService>();
+builder.Services.AddScoped<UsuarioService>();
+
+builder.Services.AddScoped<AjudaRealizadaRepository>();
+builder.Services.AddScoped<RegiaoRepository>();
+builder.Services.AddScoped<NivelUrgenciaRepository>();
+builder.Services.AddScoped<OcorrenciaRepository>();
+builder.Services.AddScoped<StatusOcorrenciaRepository>();
+builder.Services.AddScoped<TelefoneRepository>();
+builder.Services.AddScoped<TipoAjudaRepository>();
+builder.Services.AddScoped<TipoOcorrenciaRepository>();
+builder.Services.AddScoped<UsuarioRepository>();
+
+// Configura o DbContext com Oracle
 builder.Services.AddDbContext<AppDbContext>(options =>
-   options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection")));
+    options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuração do Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        c.RoutePrefix = string.Empty;  // Configura o Swagger UI para ser acessado diretamente na raiz (http://localhost:5169/)
+    });
 }
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
